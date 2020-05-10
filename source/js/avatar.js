aacbf09b1d239f1b71c4efa7677a6c3a6f10f34b
machgeek <machgeek@163.com>
2020-05-10 15:59:17 +0800

    Site updated: 2020-05-10 15:59:17

--- source/js/avatar.js
+++ source/js/avatar.js
@@ -1,5 +1,5 @@
 $(".avatar-container").hover(function(){
-    $(this).find("img").attr("src","./source/img/avatar/med2.png");
+    $(this).find("img").attr("src","https://cdn.jsdelivr.net/gh/machgeek/AAAimg/avatar/med2.webp");
 },function(){
-    $(this).find("img").attr("src","./source/img/avatar/med.png");
+    $(this).find("img").attr("src","https://cdn.jsdelivr.net/gh/machgeek/AAAimg/avatar/med.webp");
 });
