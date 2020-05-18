3a096ecf00150494ebee949b4238286066621cfe
machgeek <machgeek@163.com>
2020-05-18 23:09:55 +0800

    Site updated: 2020-05-18 23:09:55

--- source/js/gotop.js
+++ source/js/gotop.js
@@ -1,19 +1,29 @@
-// 上滚显示gotop，下滚消失
-$(document).ready(function(){  
-    var p=0,t=0;  
-    $(window).scroll(function(e){  
-            p = $(this).scrollTop();  
-              
-            if(t<=p){//下滚  
-              $('#gotop').show("fade"); 
-            }
-              
-            else{//上滚  
-              $('#gotop').hide("fade"); 
-            }  
-            setTimeout(function(){t = p;},0);         
-    });  
-  });  
+$(function () {             
+  //绑定滚动条事件 
+    //绑定滚动条事件 
+  $(window).bind("scroll", function () { 
+      var sTop = $(window).scrollTop(); 
+      var sTop = parseInt(sTop); 
+      if (sTop >= 130) { 
+          if (!$("#gotop").is(":visible")) { 
+              try { 
+                  $("#gotop").slideDown(); 
+              } catch (e) { 
+                  $("#gotop").show(); 
+              }                       
+          } 
+      } 
+      else { 
+          if ($("#gotop").is(":visible")) { 
+              try { 
+                  $("#gotop").slideUp(); 
+              } catch (e) { 
+                  $("#gotop").hide(); 
+              }                        
+          } 
+      }  
+  }); 
+}) 
   
   // 鼠标点击后缓慢返回顶部
   var btntop = $('#gotop');
