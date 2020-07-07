89bec6a360e02edf35120b2d8d733cb923a1405e
machgeek <machgeek@163.com>
2020-07-07 20:13:20 +0800

    Site updated: 2020-07-07 20:13:19

--- js/mobile-menu.js
+++
@@ -1,5 +0,0 @@
-$('.mobile-js-menu').on('click', () => {
-	$('.mobile-js-menu').toggleClass('active');
-	$('.mobile-js-nav').toggleClass('open');
-	$('.mobile-js-nav__list').toggleClass('show');
-});
